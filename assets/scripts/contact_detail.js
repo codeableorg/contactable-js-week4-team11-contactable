d8cd96061cbf0e13f5791ab5f381aafa8c865217
christian <cristian14021997@gmail.com>
2021-02-06 12:31:45 -0500

    Create render of contact-detail and added styles in contact

---
+++ assets/scripts/contact_detail.js
@@ -0,0 +1,34 @@
+export default function Login(parentElement) {
+  return {
+    parent: document.querySelector(parentElement),
+    render: function()
+    {
+      const html = `
+      <header>
+        <h2>Contact Detail</h2>
+        <a>Logout</a>
+      </header>
+      <section class="inputs">
+        <img src="./assets/img/default.png" alt="">
+        <p class="contact__name">${contact.name}</p>
+        <p>Friends</p>
+        <div contact__detail>
+        <label>Number</label>
+          <p class="contact__number">${contact.number}</p>
+        <label>Email</label>
+          <p class="contact__email">${contact.email}</p>
+        </div>
+      </section>
+      <footer>
+        <div class="options">
+          <a href="">Back</a>
+          <a href="">Delete</a>
+          <a href="">Edit</a>
+        </div>
+      </footer>
+      `;
+      this.parent.innerHTML = html;
+      this.addFormSubmitListener();
+    },
+  };
+}
